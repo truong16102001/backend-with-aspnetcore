465556199aef2c3614896a7ac530a251bcf29eb5
TruongND <truongx62001@gmail.com>
2026-05-11 09:42:32 +0700

    middleware in aspnet core

---
+++ NetCore.API/CustomMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace NetCore.API
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CustomMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+        public Task Invoke(HttpContext context) {
+            //return context.Response.WriteAsync("Hello world");
+            context.Response.Headers.Add("hackerby", "NDT");
+            return _next(context);
+        }
+    }
+}
---
+++ NetCore.API/CustomMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCore.API
+{
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
